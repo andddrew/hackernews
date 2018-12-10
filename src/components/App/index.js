60647d75977b095f8ca9c98b4c5a8b6bec7c8fa8
Andrew Kwak <andrewebdevelop@gmail.com>
2018-12-10 13:52:12 -0500

    initial commit

---
+++ src/components/App/index.js
@@ -0,0 +1,156 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { sortBy } from 'lodash';
+import './App.css';
+
+import Table from 'components/Table';
+import Search from 'components/Search';
+import Button from 'components/Button';
+
+import {
+  DEFAULT_QUERY,
+  DEFAULT_HPP,
+  PATH_BASE,
+  PATH_SEARCH,
+  PARAM_SEARCH,
+  PARAM_PAGE,
+  PARAM_HPP
+} from '../../constants';
+
+const Loading = () => <div>Loading...</div>
+
+const withLoading = ( Component ) => ({ isLoading, ...rest }) =>
+  isLoading 
+  ? <Loading />
+  : <Component { ...rest } />
+
+const ButtonWithLoading = withLoading( Button );
+
+class App extends Component {
+  state = {
+    results: null,
+    searchKey: '',
+    searchTerm: DEFAULT_QUERY,
+    error: null,
+    isLoading: false,
+  }
+  
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+    axios.get( `${ PATH_BASE }${ PATH_SEARCH }?${ PARAM_SEARCH }${ searchTerm }` )
+      .then( results => this.setSearchTopStories( results.data ) )
+      .catch ( error => this.setState({ error }) );
+  }
+
+  setSearchTopStories = result => {
+    const { hits, page } = result;
+    const { searchKey, results } = this.state;
+
+    const oldHits = results && results[searchKey]
+      ? results[searchKey].hits
+      : [];
+
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+    this.setState({
+      results: {
+        ...results,
+        [searchKey]: {
+          hits: updatedHits,
+          page
+        },
+        isLoading: false,
+      }
+    });
+  }
+
+  needsToSearchTopStories = ( searchTerm ) => {
+    return !this.state.results[searchTerm];
+  }
+
+  fetchSearchTopStories = ( searchTerm, page = 0 ) => {
+    this.setState({ isLoading: true });
+    fetch( `${ PATH_BASE }${ PATH_SEARCH }?${ PARAM_SEARCH }${ searchTerm }&${ PARAM_PAGE }${ page }&${ PARAM_HPP }${ DEFAULT_HPP}` )
+      .then( response => response.json() )
+      .then( results => this.setSearchTopStories( results ) )
+      .catch( error => { throw new Error('Error query: ' + error ) } )
+  }
+
+  onSearchSubmit = ( event ) => {
+    const { searchTerm } = this.state;
+    this.setState({ searchKey: searchTerm });
+
+    if ( this.needsToSearchTopStories( searchTerm ) ) {
+      this.fetchSearchTopStories( searchTerm );
+    }
+    event.preventDefault();
+  }
+
+  onSearchChange = ( event ) => {
+    this.setState( { searchTerm: event.target.value } );
+  }
+
+  onDismiss = ( id ) => {
+    const { searchKey, results } = this.state;
+    const { hits, page } = results[searchKey];
+    const isNotId = item => item.objectID !== id;
+    const updatedHits = hits.filter( isNotId );
+ 
+    this.setState( {
+      results: {
+        ...results,
+        [searchKey]: { hits: updatedHits, page }
+      }
+    });
+  }
+
+  render() {
+    const { searchTerm, results, searchKey, error, isLoading } = this.state;
+
+    if ( error ) {
+      return <p>Something went wrong!</p>;
+    }
+
+    const page = (
+      results &&
+      results[searchKey] &&
+      results[searchKey].page
+    ) || 0;
+
+    const list = (
+      results &&
+      results[searchKey] &&
+      results[searchKey].hits
+    ) || [];
+
+    return (
+      <div className="page">
+        <div className="interactions">
+          <Search
+            value={ searchTerm }
+            onChange={ this.onSearchChange }
+            onSubmit={ this.onSearchSubmit }
+          >Search</Search>
+        </div>
+        { results &&
+          <Table
+            list={ list }
+            onDismiss={ this.onDismiss }
+          />
+        }
+        <div className="interactions">
+          <ButtonWithLoading
+            isLoading={ isLoading }
+            onClick={ () => this.fetchSearchTopStories( searchKey, page+1 ) }>
+              More
+            </ButtonWithLoading>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
